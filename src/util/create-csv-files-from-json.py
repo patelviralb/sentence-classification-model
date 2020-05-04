dcdfd632bed439cb4104a2bd4af481f46b13a114
Viral Patel <patel.viral@northeastern.edu>
2020-05-03 22:06:42 -0400

    Renamed variables

--- src/util/create-csv-files-from-json.py
+++ src/util/create-csv-files-from-json.py
@@ -1,76 +1,77 @@
 import json
 import string
 import re
+
+
 # import os
 
 
-def createDataFiles(inputTrainingJSONFiles, inputTestJSONFiles):
-    trainingJSONFiles = inputTrainingJSONFiles
-    testJSONFiles = inputTestJSONFiles
+def create_data_files(input_training_json_files, input_test_json_files):
+    training_json_files = input_training_json_files
+    test_json_files = input_test_json_files
 
-    print ("Started File Conversion....")
+    print("Started File Conversion....")
     # print ("Working Directory: ",os.getcwd())
-    
-    trainingFiles = readFile(trainingJSONFiles)
-    testingFiles = readFile(testJSONFiles)
 
-    return trainingFiles, testingFiles
+    training_files = read_file(training_json_files)
+    testing_files = read_file(test_json_files)
+
+    return training_files, testing_files
 
 
-def readFile(fileDictionary):
+def read_file(file_dictionary):
     printable = set(string.printable)
-    textFileDictionary = {}
+    text_file_dictionary = {}
 
-
-    for source,filePath in fileDictionary.items():
+    for source, filePath in file_dictionary.items():
         # print("Source: ",source,"\tFile Path: ",filePath)
-        fileName = filePath.split("/")[2].split(".")[0]
-        csvFilename = "data-files/" + fileName + ".txt"
-        # print("CSV File: ",csvFilename)
-        f = open(csvFilename,'w+')
+        filename = filePath.split("/")[2].split(".")[0]
+        text_filename = "data-files/" + filename + ".txt"
+        # print("CSV File: ",text_filename)
+        f = open(text_filename, 'w+')
 
-        currentIndex = list(fileDictionary.keys()).index(source)
-        # print("currentIndex: ",currentIndex)
+        current_index = list(file_dictionary.keys()).index(source)
+        # print("current_index: ",current_index)
 
-        textFileDictionary[source] = csvFilename
-        # print(trainingFiles)
+        text_file_dictionary[source] = text_filename
+        # print(training_files)
 
-        with open(filePath) as inputJSONFile:
-            jsonObject = json.load(inputJSONFile)
-            intentData = jsonObject[source]
-            
-            for eachData in intentData:
+        with open(filePath) as input_json_file:
+            json_object = json.load(input_json_file)
+            intent_data = json_object[source]
+
+            for eachData in intent_data:
                 data = eachData["data"]
-                textData = ""
+                text_data = ""
                 for sentenceText in range(len(data)):
-                    textData = textData + data[sentenceText]["text"] + " "
-                # print(str(textData).strip())
-                printableTextData = ''.join(filter(lambda x: x in printable, str(textData).strip()))
-                filteredTextData = re.sub(' +', ' ', re.sub('[^\w\s]', '', printableTextData))
-                dataToAdd = filteredTextData + "\t" + str(currentIndex) + "\n"
-                f.write(dataToAdd)
-        
-    return textFileDictionary
+                    text_data = text_data + data[sentenceText]["text"] + " "
+                # print(str(text_data).strip())
+                printable_text_data = ''.join(filter(lambda x: x in printable, str(text_data).strip()))
+                filtered_text_data = re.sub(' +', ' ', re.sub('[^\w\s]', '', printable_text_data))
+                data_to_add = filtered_text_data + "\t" + str(current_index) + "\n"
+                f.write(data_to_add)
 
-inputTrainingJSONFiles = {
-    'AddToPlaylist':'data-files/json/train_AddToPlaylist_full.json',
-    'BookRestaurant':'data-files/json/train_BookRestaurant_full.json',
-    'GetWeather':'data-files/json/train_GetWeather_full.json',
-    'PlayMusic':'data-files/json/train_PlayMusic_full.json',
-    'SearchCreativeWork':'data-files/json/train_SearchCreativeWork_full.json',
-    'SearchScreeningEvent':'data-files/json/train_SearchScreeningEvent_full.json'
-}
+    return text_file_dictionary
 
-inputTestJSONFiles = {
-    'AddToPlaylist':'data-files/json/validate_AddToPlaylist.json',
-    'BookRestaurant':'data-files/json/validate_BookRestaurant.json',
-    'GetWeather':'data-files/json/validate_GetWeather.json',
-    'PlayMusic':'data-files/json/validate_PlayMusic.json',
-    'SearchCreativeWork':'data-files/json/validate_SearchCreativeWork.json',
-    'SearchScreeningEvent':'data-files/json/validate_SearchScreeningEvent.json'
-}
+# input_training_json_files = {
+#     'AddToPlaylist':'data-files/json/train_AddToPlaylist_full.json',
+#     'BookRestaurant':'data-files/json/train_BookRestaurant_full.json',
+#     'GetWeather':'data-files/json/train_GetWeather_full.json',
+#     'PlayMusic':'data-files/json/train_PlayMusic_full.json',
+#     'SearchCreativeWork':'data-files/json/train_SearchCreativeWork_full.json',
+#     'SearchScreeningEvent':'data-files/json/train_SearchScreeningEvent_full.json'
+# }
+#
+# input_test_json_files = {
+#     'AddToPlaylist':'data-files/json/validate_AddToPlaylist.json',
+#     'BookRestaurant':'data-files/json/validate_BookRestaurant.json',
+#     'GetWeather':'data-files/json/validate_GetWeather.json',
+#     'PlayMusic':'data-files/json/validate_PlayMusic.json',
+#     'SearchCreativeWork':'data-files/json/validate_SearchCreativeWork.json',
+#     'SearchScreeningEvent':'data-files/json/validate_SearchScreeningEvent.json'
+# }
 
-trainingFiles, testingFiles = createDataFiles(inputTrainingJSONFiles, inputTestJSONFiles)
-print("Finished....")
-print(trainingFiles)
-print(testingFiles)
+# training_files, testing_files = create_data_files(input_training_json_files, input_test_json_files)
+# print("Finished....")
+# print(training_files)
+# print(testing_files)
