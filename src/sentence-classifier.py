0f59768847152ee55d42201c0b9bb0130ef748f8
Viral Patel <patel.viral@northeastern.edu>
2020-05-04 00:10:10 -0400

    Completed Base Model run using Logictic Regression

--- src/sentence-classifier.py
+++ src/sentence-classifier.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 
 def create_data_frame_list(data_file_dict):
@@ -13,25 +15,31 @@ def create_data_frame_list(data_file_dict):
     return data_frame_list
 
 
+def run_base_model(train_sentences, training_intents, test_sentences, testing_intents):
+    classifier = LogisticRegression()
+    classifier.fit(train_sentences, training_intents)
+    return classifier.score(test_sentences, testing_intents)
+
+
 def run_model():
     print("Model Started....")
 
     input_training_files = {
         'AddToPlaylist': '../data-files/train_AddToPlaylist_full.txt',
-        # 'BookRestaurant': '../data-files/train_BookRestaurant_full.txt',
-        # 'GetWeather': '../data-files/train_GetWeather_full.txt',
-        # 'PlayMusic': '../data-files/train_PlayMusic_full.txt',
-        # 'SearchCreativeWork': '../data-files/train_SearchCreativeWork_full.txt',
-        # 'SearchScreeningEvent': '../data-files/train_SearchScreeningEvent_full.txt'
+        'BookRestaurant': '../data-files/train_BookRestaurant_full.txt',
+        'GetWeather': '../data-files/train_GetWeather_full.txt',
+        'PlayMusic': '../data-files/train_PlayMusic_full.txt',
+        'SearchCreativeWork': '../data-files/train_SearchCreativeWork_full.txt',
+        'SearchScreeningEvent': '../data-files/train_SearchScreeningEvent_full.txt'
     }
 
     input_test_files = {
         'AddToPlaylist': '../data-files/validate_AddToPlaylist.txt',
-        # 'BookRestaurant': '../data-files/validate_BookRestaurant.txt',
-        # 'GetWeather': '../data-files/validate_GetWeather.txt',
-        # 'PlayMusic': '../data-files/validate_PlayMusic.txt',
-        # 'SearchCreativeWork': '../data-files/validate_SearchCreativeWork.txt',
-        # 'SearchScreeningEvent': '../data-files/validate_SearchScreeningEvent.txt'
+        'BookRestaurant': '../data-files/validate_BookRestaurant.txt',
+        'GetWeather': '../data-files/validate_GetWeather.txt',
+        'PlayMusic': '../data-files/validate_PlayMusic.txt',
+        'SearchCreativeWork': '../data-files/validate_SearchCreativeWork.txt',
+        'SearchScreeningEvent': '../data-files/validate_SearchScreeningEvent.txt'
     }
 
     training_data_frame_list = create_data_frame_list(input_training_files)
@@ -42,13 +50,21 @@ def run_model():
 
     training_sentences = training_data_set['sentence'].values
     training_intents = training_data_set['intent'].values
-
-    test_sentences = test_data_set['sentence'].values
-    test_intents = test_data_set['intent'].values
+    testing_sentences = test_data_set['sentence'].values
+    testing_intents = test_data_set['intent'].values
 
     sentences_train, sentences_test, y_train, y_test = train_test_split(training_sentences, training_intents,
                                                                         test_size=0.25,
                                                                         random_state=1000)
+
+    # Word Vectorization
+    vectorizer = CountVectorizer()
+    vectorizer.fit(training_sentences)
+    train_sentences = vectorizer.transform(training_sentences)
+    test_sentences = vectorizer.transform(testing_sentences)
+
+    base_accuracy = run_base_model(train_sentences, training_intents, test_sentences, testing_intents)
+    print("Base Model Accuracy:", base_accuracy)
 
 
 if __name__ == '__main__':
